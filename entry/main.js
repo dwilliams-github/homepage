1d4cd64318c94002b4b82e68d185f042cfab2621
David Williams <dwilliams@slashdave.com>
2019-09-22 22:20:37 -0700

    Working framework for data entry electron app

---
+++ entry/main.js
@@ -0,0 +1,61 @@
+const {app, BrowserWindow} = require('electron')
+const cluster = require('cluster');
+const access = require('./data/access');
+
+if (cluster.isMaster) {
+    const worker = cluster.fork();
+
+    //
+    // Main electron process
+    //
+    let win = null;
+
+    function createWindow() {
+        // Initialize the window to our specified dimensions
+        win = new BrowserWindow({
+            width: 1000, 
+            height: 600,
+            webPreferences: {
+                nodeIntegration: true       // blueprintjs expects a process object
+            }
+        });
+
+        // Specify entry point
+        win.loadFile('index.html');
+
+        // Show dev tools
+        // Remove this line before distributing
+        win.webContents.openDevTools()
+
+        // Remove window once app is closed
+        win.on('closed', function () {
+            win = null;
+        });
+    }
+
+    app.on('ready', function () {
+        createWindow();
+    });
+
+    app.on('activate', () => {
+        if (win === null) {
+            createWindow()
+        }
+    })
+
+    app.on('window-all-closed', function () {
+        if (process.platform != 'darwin') {
+            app.quit();
+        }
+    });
+
+    app.on('quit', function() {
+        worker.kill();
+    });
+
+} else {
+    //
+    // Our database httpd server
+    //
+    access.service(8000);
+}
---
+++ entry/src/Venue.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+class Venue extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                Venue
+            </div>
+        )
+    }
+}
+
+export default Venue;
